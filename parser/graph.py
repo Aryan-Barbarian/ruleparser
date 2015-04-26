e64b1809057ec4123fd3d67bad8ebfba714a0c20
Aryan Falahatpisheh <aryan.falahatpisheh@berkeley.edu>
2015-04-26 11:27:09 -0700

    Working on adding utility functions to NativeGraph

--- parser/graph.py
+++ parser/graph.py
@@ -26,18 +26,37 @@ class NativeGraph(object):
 		self._inner_graph = nx.Graph();
 		self.nodes = dict(); # maps node.hash (hash int) => node (NativeNode)
 
-	def set_sibling_dist(self, n1, n2, dist):
-
-		pass
 
 	"""
 	Takes in N1, N2, which are NativeNode objects.
 	Inserts them to our nx graph and does the work of conerting from
 		NativeNode to an nx node
 	"""
-	def add_edge(self, n1, n2):
+	def add_edge(self, n1, n2, attribs):
 
-	def add_node(self, node)
+		if not self.has_node(n1):
+			self.add_node(n1);
+		if not self.has_node(n2):
+			self.add_node(n2);
+
+		# TODO: does this hash them correctly?
+		self._inner_graph.add_edge(n1, n2, attr_dict=attribs);
+
+	def add_sibling_edge(self, n1, n2, dist):
+		attribs = {"type":"sibling", "dist":dist}
+		self.add_edge(n1, n2, attribs)
+
+	def add_parent_edge(self, n1, n2, dist):
+		attribs = {"type":"parent"}
+		self.add_edge(n1, n2, attribs)
+
+
+	def add_node(self, node):
+		# TODO: at risk of duplicating?
+		attribs = node.get_attribute_dict();
+
+	def has_node(self, node):
+
 
 
 class NativeNode(object):
@@ -59,7 +78,7 @@ class NativeNode(object):
 
 	def add_child(self, child):
 		child.parent = self
-		self.graph.add_edge(self, child, type="parent")
+		self.graph.add_parent_edge(self, child)
 
 	def process_many(self, html_list):
 		children = list();
@@ -72,7 +91,7 @@ class NativeNode(object):
 				old = children[i]
 				index_to_add = len(children);
 				dist = index_to_add - old - 1;
-				self.graph.set_sibling_dist(old, node, dist);
+				self.graph.add_sibling_edge(old, node, dist)
 
 			self.add_child(node);
 
